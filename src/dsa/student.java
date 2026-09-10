90eb2ba8d4c0d47e2beb3ef0895d3e8abcab4155
dikshabhor79 <dikshabhor79@gmail.com>
2026-09-10 20:00:09 +0530

    add student.java file

---
+++ src/dsa/student.java
@@ -0,0 +1,9 @@
+package dsa;
+
+public class student {
+	public static void main(String[] args) {
+		   System.out.println("Hello GitHub!");
+	        System.out.println("My Java project is connected with GitHub.");
+		
+	}
+}
